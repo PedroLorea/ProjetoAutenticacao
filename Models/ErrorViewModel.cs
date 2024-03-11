d176cded88d5f84a9c16469e53abb5aa110dd097
PedroLorea <p.lorea@edu.pucrs.br>
2024-03-11 16:58:40 -0300

    dotnet new mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjetoAutenticacao.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
